652858f6dc3c84552ac6b3a959e61800406f41a4
Caio Fernando Bertoldi Paes de Andrade <caiofbpa@icloud.com>
2018-01-18 08:57:37 -0200

    Use SKRTMAPI poiting to cross-platform starscream

--- Package.swift
+++ Package.swift
@@ -1,14 +1,15 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "SlackKit",
-    targets: [
-        Target(name: "SlackKit")
-    ],
     dependencies: [
-        .Package(url: "https://github.com/SlackKit/SKCore", majorVersion: 4),
-        .Package(url: "https://github.com/SlackKit/SKClient", majorVersion: 4),
-        .Package(url: "https://github.com/SlackKit/SKRTMAPI", majorVersion: 4),
-        .Package(url: "https://github.com/SlackKit/SKServer", majorVersion: 4)
+        .package(url: "https://github.com/SlackKit/SKCore", .upToNextMajor(from: "4.0.0")),
+        .package(url: "https://github.com/SlackKit/SKClient", .upToNextMajor(from: "4.0.0")),
+        .package(url: "https://github.com/caiofbpa/SKRTMAPI", .branch("use-cross-platform-starscream")),
+        .package(url: "https://github.com/SlackKit/SKServer", .upToNextMajor(from: "4.0.0"))
+    ],
+    targets: [
+        .target(name: "SlackKit", dependencies: ["SKClient", "SKRTMAPI", "SKServer"], path: "Sources")
     ]
 )
